7c8e8e8f1575224d28821478415dffc258cb756f
Marcin <marcin.olejniczak@doradigital.pl>
2017-04-22 00:50:20 +0200

    Final script

--- linear_solver.py
+++ linear_solver.py
@@ -1,38 +1,73 @@
 from pulp import *
 
-# declare your variables
-x1 = LpVariable("x1", 0, 1)  # 0<= x1 <= 40
-x2 = LpVariable("x2", 0, 1)  # 0<= x2 <= 1000
+def print_mixed_strategy(payoff_matrix, optimalization=LpMinimize):
+    """
+    Minimize for player A - row,
+    Maximaze for player B - columns
+    :param payoff_matrix:
+    :param optimalization:
+    :return:
+    """
+    # calc for rows
+    variables = []
+    # declare your variables
+    for i, row in enumerate(payoff_matrix):
+        variables.append(LpVariable("x{}".format(i), 0, 1))
 
-# defines the problem
-prob = LpProblem("problem", LpMinimize)
+    # defines the problem
+    prob = LpProblem("problem", optimalization)
 
-# defines the constraints
-prob += (5 * x1) + (2 * x2) >= 1
-prob += (0 * x1) + (4 * x2) >= 1
-prob += (1 * x1) + (3 * x2) >= 1
+    # defines the constraints
+    # transponse matrix to write conditions easier
+    transponsed_matrix = [list(i) for i in zip(*payoff_matrix)]
+    for row in transponsed_matrix:
+        row_prob = 0
+        for i, factor in enumerate(row):
+            row_prob += factor * variables[i]
+        if optimalization is LpMinimize:
+            prob += row_prob >= 1
+        else:
+            prob += row_prob <= 1
 
-#prob += x1 > 0 these 2 conditions are probably not necessary
-#prob += x2 > 0
+    # defines the objective function to min/max-ize
+    prob += sum(variables)
 
-# defines the objective function to maximize
-prob += x1 + x2
+    # solve the problem
+    status = prob.solve(GLPK(msg=0))
+    LpStatus[status]
 
-# solve the problem
-status = prob.solve(GLPK(msg=0))
-LpStatus[status]
+    var_values = []
+    for variable in variables:
+        var_value = value(variable)
+        print "{}': {}".format(
+            variable.name, value(variable)
+        )
+        var_values.append(var_value)
 
-# print the results x1 = 20, x2 = 60
-x1 = value(x1)
-x2 = value(x2)
+    v = 1 / sum(var_values)
+    print 'V: {}'.format(v)
+    # actually it's what we need to determine - how often play given strategy
+    frequencies = []
+    for var in variables:
+        print '{}: {}'.format(var.name, value(var) * v)
+# test
+payoff_matrix = [
+    [5, 0, 1],
+    [2, 4, 3],
+]
+print 'Row Player Strategy'
+print_mixed_strategy(payoff_matrix, LpMinimize)
+print 'Column Player Strategy'
+print_mixed_strategy([list(i) for i in zip(*payoff_matrix)], LpMaximize)
 
-print "X1':{},X2':{},".format(
-    x1, x2,
-)
 
-v = 1 / (x1 + x2)
-f1 = x1 * v
-f2 = x2 * v
-print 'V:{}, X1:{}, X2:{}'.format(
-    v, f1, f2,
-)
+# test for our variant
+payoff_matrix = [
+    [1.2, 0.4, ],
+    [0.2, 1, ],
+    [0.8, 0.8, ],
+]
+print 'Row Player Strategy'
+print_mixed_strategy(payoff_matrix, LpMinimize)
+print 'Column Player Strategy'
+print_mixed_strategy([list(i) for i in zip(*payoff_matrix)], LpMaximize)
